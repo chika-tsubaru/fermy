d1da650b1fd1db07482a060ec73d10f422d478e3
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2021-01-05 15:26:14 +0900

    complete quest test

--- spec/controllers/quests_controller_spec.rb
+++ spec/controllers/quests_controller_spec.rb
@@ -7,12 +7,73 @@ describe QuestsController do
     context 'log in' do
       before do
         login user
+        get :new
       end
 
       it "renders the :new template" do
-        login user
-        get :new
         expect(response).to render_template :new
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :new
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    let(:params) { { user_id: user.id, quest: attributes_for(:quest) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up quest' do
+          expect{ subject }.to change(Quest, :count).by(1)
+        end
+
+        it 'redirects to root_path' do
+          subject
+          expect(response).to redirect_to(root_path)
+        end
+      end
+
+      context 'can not save' do
+        let(:invalid_params) { { user_id: user.id, quest: attributes_for(:quest, title: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Quest, :count)
+        end
+
+        it 'renders new_quest' do
+          subject
+          expect(response).to redirect_to(new_quest_path)
+        end
+      end
+    end
+
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
@@ -27,6 +88,49 @@ describe QuestsController do
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+
+    it "select quest successfully" do
+      quest = create(:quest)
+      get :show, params: { id: quest.id }
+      expect(assigns(:quest)).to eq(quest)
+    end
+
+    it "renders the :show template" do
+      quest = create(:quest)
+      get :show, params: { id: quest.id }
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:params) { { user_id: user.id, quest: attributes_for(:quest) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      it 'count down quest' do
+        quest = create(:quest)
+        expect{ delete :destroy, params: { id: quest.id } }.to change(Quest, :count).by(-1)
+      end
+
+      it 'redirects to root_path' do
+        quest = create(:quest)
+        expect( delete :destroy, params: { id: quest.id } ).to redirect_to(root_path)
+      end
+    end
+
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        quest = create(:quest)
+        expect( delete :destroy, params: { id: quest.id } ).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
